14459f91bd5e2a344149c3c725f032276fc8e60e
AnthonyCheetham <anthony.cheetham@unige.ch>
2019-10-05 05:58:18 +1000

    Cleaned up image classification code. Moved common functions to a separate library

---
+++ image_classification/image_classification_tools.py
@@ -0,0 +1,291 @@
+import torch, torchvision
+from torchvision import datasets, models, transforms
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import time
+from torchsummary import summary
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# Applying Transforms to the Data
+def make_transforms():
+    """ Makes image transforms to get input data into the right format
+    for resnet / pytorch"""
+    image_transforms = { 
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)),
+            transforms.RandomRotation(degrees=15),
+            transforms.RandomHorizontalFlip(),
+            transforms.CenterCrop(size=224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ]),
+        'valid': transforms.Compose([
+            transforms.Resize(size=256),
+            transforms.CenterCrop(size=224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ]),
+        'test': transforms.Compose([
+            transforms.Resize(size=256),
+            transforms.CenterCrop(size=224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ])
+    }
+    return image_transforms
+
+def train_and_validate(model, loss_criterion, optimizer,train_data_loader,valid_data_loader,
+                       train_data_size,valid_data_size,epochs=25,dataset=''):
+    '''
+    Function to train and validate
+    Parameters
+        :param model: Model to train and validate
+        :param loss_criterion: Loss Criterion to minimize
+        :param optimizer: Optimizer for computing gradients
+        :param epochs: Number of epochs (default=25)
+  
+    Returns
+        model: Trained Model with best validation accuracy
+        history: (dict object): Having training loss, accuracy and validation loss, accuracy
+    '''
+    
+    start = time.time()
+    history = []
+    best_acc = 0.0
+    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    for epoch in range(epochs):
+        epoch_start = time.time()
+        print("Epoch: {}/{}".format(epoch+1, epochs))
+        
+        # Set to training mode
+        model.train()
+        
+        # Loss and Accuracy within the epoch
+        train_loss = 0.0
+        train_acc = 0.0
+        
+        valid_loss = 0.0
+        valid_acc = 0.0
+        
+        for i, (inputs, labels) in enumerate(train_data_loader):
+
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            
+            # Clean existing gradients
+            optimizer.zero_grad()
+            
+            # Forward pass - compute outputs on input data using the model
+            outputs = model(inputs)
+            
+            # Compute loss
+            loss = loss_criterion(outputs, labels)
+            
+            # Backpropagate the gradients
+            loss.backward()
+            
+            # Update the parameters
+            optimizer.step()
+            
+            # Compute the total loss for the batch and add it to train_loss
+            train_loss += loss.item() * inputs.size(0)
+            
+            # Compute the accuracy
+            ret, predictions = torch.max(outputs.data, 1)
+            correct_counts = predictions.eq(labels.data.view_as(predictions))
+            
+            # Convert correct_counts to float and then compute the mean
+            acc = torch.mean(correct_counts.type(torch.FloatTensor))
+            
+            # Compute total accuracy in the whole batch and add to train_acc
+            train_acc += acc.item() * inputs.size(0)
+            
+            #print("Batch number: {:03d}, Training: Loss: {:.4f}, Accuracy: {:.4f}".format(i, loss.item(), acc.item()))
+
+            
+        # Validation - No gradient tracking needed
+        with torch.no_grad():
+
+            # Set to evaluation mode
+            model.eval()
+
+            # Validation loop
+            for j, (inputs, labels) in enumerate(valid_data_loader):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # Forward pass - compute outputs on input data using the model
+                outputs = model(inputs)
+
+                # Compute loss
+                loss = loss_criterion(outputs, labels)
+
+                # Compute the total loss for the batch and add it to valid_loss
+                valid_loss += loss.item() * inputs.size(0)
+
+                # Calculate validation accuracy
+                ret, predictions = torch.max(outputs.data, 1)
+                correct_counts = predictions.eq(labels.data.view_as(predictions))
+
+                # Convert correct_counts to float and then compute the mean
+                acc = torch.mean(correct_counts.type(torch.FloatTensor))
+
+                # Compute total accuracy in the whole batch and add to valid_acc
+                valid_acc += acc.item() * inputs.size(0)
+
+                #print("Validation Batch number: {:03d}, Validation: Loss: {:.4f}, Accuracy: {:.4f}".format(j, loss.item(), acc.item()))
+            
+        # Find average training loss and training accuracy
+        avg_train_loss = train_loss/train_data_size 
+        avg_train_acc = train_acc/train_data_size
+
+        # Find average training loss and training accuracy
+        avg_valid_loss = valid_loss/valid_data_size 
+        avg_valid_acc = valid_acc/valid_data_size
+
+        history.append([avg_train_loss, avg_valid_loss, avg_train_acc, avg_valid_acc])
+                
+        epoch_end = time.time()
+    
+        print("Epoch : {:03d}, Training: Loss: {:.4f}, Accuracy: {:.4f}%, \n\t\tValidation : Loss : {:.4f}, Accuracy: {:.4f}%, Time: {:.4f}s".format(epoch, avg_train_loss, avg_train_acc*100, avg_valid_loss, avg_valid_acc*100, epoch_end-epoch_start))
+        
+        # Save if the model has best accuracy till now
+        torch.save(model, dataset+'_model_'+str(epoch)+'.pt')
+            
+    return model, history
+    
+def computeTestSetAccuracy(model, loss_criterion):
+    '''
+    Function to compute the accuracy on the test set
+    Parameters
+        :param model: Model to test
+        :param loss_criterion: Loss Criterion to minimize
+    '''
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    test_acc = 0.0
+    test_loss = 0.0
+    
+    all_predictions = []
+
+    # Validation - No gradient tracking needed
+    with torch.no_grad():
+
+        # Set to evaluation mode
+        model.eval()
+
+        # Validation loop
+        for j, (inputs, labels) in enumerate(test_data_loader):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # Forward pass - compute outputs on input data using the model
+            outputs = model(inputs)
+
+            # Compute loss
+            loss = loss_criterion(outputs, labels)
+
+            # Compute the total loss for the batch and add it to valid_loss
+            test_loss += loss.item() * inputs.size(0)
+
+            # Calculate validation accuracy
+            ret, predictions = torch.max(outputs.data, 1)
+            correct_counts = predictions.eq(labels.data.view_as(predictions))
+            all_predictions.append(predictions)
+
+            # Convert correct_counts to float and then compute the mean
+            acc = torch.mean(correct_counts.type(torch.FloatTensor))
+
+            # Compute total accuracy in the whole batch and add to valid_acc
+            test_acc += acc.item() * inputs.size(0)
+
+            print("Test Batch number: {:03d}, Test: Loss: {:.4f}, Accuracy: {:.4f}".format(j, loss.item(), acc.item()))
+
+    # Find average test loss and test accuracy
+    avg_test_loss = test_loss/test_data_size 
+    avg_test_acc = test_acc/test_data_size
+
+    print("Test accuracy : " + str(avg_test_acc))
+    return all_predictions
+
+
+def predict(model, test_image_name,image_transforms,idx_to_class,silent=False,return_scores=False):
+    '''
+    Function to predict the class of a single test image
+    Parameters
+        :param model: Model to test
+        :param test_image_name: Test image
+
+    '''
+    
+    transform = image_transforms['test']
+
+    test_image = Image.open(test_image_name)
+    plt.imshow(test_image)
+    
+    test_image_tensor = transform(test_image)
+
+    if torch.cuda.is_available():
+        test_image_tensor = test_image_tensor.view(1, 3, 224, 224).cuda()
+    else:
+        test_image_tensor = test_image_tensor.view(1, 3, 224, 224)
+    
+    with torch.no_grad():
+        model.eval()
+        # Model outputs log probabilities
+        out = model(test_image_tensor)
+        ps = torch.exp(out)
+        topk, topclass = ps.topk(3, dim=1)
+        if not silent:
+            for i in range(3):
+                print("Prediction", i+1, ":", idx_to_class[topclass.cpu().numpy()[0][i]], ", Score: ", topk.cpu().numpy()[0][i])
+        top_classes = topclass.cpu().numpy()[0]
+        top_scores = topk.cpu().numpy()[0]
+    if return_scores:
+        return [top_classes,top_scores]
+    else:
+        return top_classes[0]
+
+
+def plot_confusion_matrix(conf_matrix,cats=[],save=False,
+                         cmap=plt.cm.Blues):
+    n_classes = len(cats)
+    
+    # Format the class names
+    class_names = [c.replace('_',' ') for c in cats]
+    
+    plt.clf()
+    plt.imshow(conf_matrix,cmap=cmap)
+    fmt = '.2f'
+    thresh = conf_matrix.max() / 2.
+    for i in range(conf_matrix.shape[0]):
+        for j in range(conf_matrix.shape[1]):
+            if conf_matrix[i,j] > thresh:
+                col = "white"
+            else:
+                col = 'black'
+            
+            plt.gca().text(j, i, format(conf_matrix[i, j], fmt),
+                    ha="center", va="center",color=col)
+    plt.xticks(np.arange(n_classes),class_names,rotation=45,ha='right')
+    plt.yticks(np.arange(n_classes),class_names)
+    plt.xlim(-0.5,n_classes-0.5)
+    plt.ylim(n_classes-0.5,-0.5)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted')
+    plt.tight_layout()
+    #
+    if save:
+        plt.savefig(save,dpi=300)
+    plt.show()
